f6b3f935c119ec436a72ca518a5dfb3a44e8ee27
赵轶 <zhaoyi_dsx@163.com>
2022-03-18 23:11:23 +0800

    将消费者剥离成pull和push两个消费者

---
+++ src/Berry.Spider.Push.Consumers/SpiderConsumersModule.cs
@@ -0,0 +1,37 @@
+﻿using Berry.Spider.Baidu;
+using Berry.Spider.TouTiao;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Berry.Spider.EntityFrameworkCore;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.EventBus.RabbitMq;
+using Volo.Abp.Modularity;
+
+namespace Berry.Spider.Consumers;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpEventBusRabbitMqModule),
+    typeof(SpiderEntityFrameworkCoreModule),
+    //今日头条模块
+    typeof(TouTiaoSpiderModule),
+    //百度模块
+    typeof(BaiduSpiderModule)
+)]
+public class SpiderConsumersModule : AbpModule
+{
+    public override Task OnApplicationInitializationAsync(ApplicationInitializationContext context)
+    {
+        var logger = context.ServiceProvider.GetRequiredService<ILogger<SpiderConsumersModule>>();
+        var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
+
+        var hostEnvironment = context.ServiceProvider.GetRequiredService<IHostEnvironment>();
+        logger.LogInformation($"EnvironmentName => {hostEnvironment.EnvironmentName}");
+
+        return Task.CompletedTask;
+    }
+}
