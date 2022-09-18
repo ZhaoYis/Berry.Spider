743dd8a8c6561aee56ac6450e07c3156e2ed8e0e
大師兄丶 <zhaoyi_dsx@163.com>
2022-09-18 22:06:31 +0800

    feat: redis缓存引入

--- src/Berry.Spider.Core/SpiderCoreModule.cs
+++ src/Berry.Spider.Core/SpiderCoreModule.cs
@@ -1,13 +1,17 @@
+using System.Reflection;
 using Berry.Spider.Contracts;
 using Berry.Spider.Proxy;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
 using Volo.Abp.Modularity;
 using Volo.Abp.TextTemplating.Scriban;
 
 namespace Berry.Spider.Core;
 
 [DependsOn(typeof(AbpCachingModule),
+    typeof(AbpCachingStackExchangeRedisModule),
     typeof(AbpTextTemplatingScribanModule),
     //IP代理
     typeof(SpiderProxyModule))]
@@ -24,15 +28,13 @@ public class SpiderCoreModule : AbpModule
         //配置SpiderOptions
         context.Services.Configure<SpiderOptions>(configuration.GetSection(nameof(SpiderOptions)));
         //配置HumanMachineVerificationOptions
-        context.Services.Configure<HumanMachineVerificationOptions>(
-            configuration.GetSection(nameof(HumanMachineVerificationOptions)));
+        context.Services.Configure<HumanMachineVerificationOptions>(configuration.GetSection(nameof(HumanMachineVerificationOptions)));
         //配置QuartzOptions
         context.Services.Configure<SpiderQuartzOptions>(configuration.GetSection(nameof(SpiderQuartzOptions)));
         //配置ExceptionlessOptions
         context.Services.Configure<ExceptionlessOptions>(configuration.GetSection(nameof(ExceptionlessOptions)));
         //配置TitleTemplateContentOptions
-        context.Services.Configure<TitleTemplateContentOptions>(
-            configuration.GetSection(nameof(TitleTemplateContentOptions)));
+        context.Services.Configure<TitleTemplateContentOptions>(configuration.GetSection(nameof(TitleTemplateContentOptions)));
         //配置AbstractTemplateOptions
         context.Services.Configure<AbstractTemplateOptions>(configuration.GetSection(nameof(AbstractTemplateOptions)));
 
@@ -48,6 +50,11 @@ public class SpiderCoreModule : AbpModule
             options.KeyPrefix = "Berry:Spider:";
             options.GlobalCacheEntryOptions.SlidingExpiration = TimeSpan.FromMinutes(10);
             options.GlobalCacheEntryOptions.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30);
+        });
+        //分布式缓存Redis
+        Configure<RedisCacheOptions>(options =>
+        {
+            options.InstanceName = Assembly.GetExecutingAssembly().FullName;
         });
 
         return Task.CompletedTask;
