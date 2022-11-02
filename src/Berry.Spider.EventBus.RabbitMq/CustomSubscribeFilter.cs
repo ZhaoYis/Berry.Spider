01d5882e8407f46c7ac78398e9b78b9580905736
大師兄丶 <zhaoyi@yzw.cn>
2022-11-02 18:01:51 +0800

    feat: 调整CAP配置

--- src/Berry.Spider.EventBus.RabbitMq/CustomSubscribeFilter.cs
+++ src/Berry.Spider.EventBus.RabbitMq/CustomSubscribeFilter.cs
@@ -2,20 +2,31 @@ using DotNetCore.CAP.Filter;
 
 namespace Berry.Spider.EventBus.RabbitMq;
 
+/// <summary>
+/// 自定义过滤器
+/// </summary>
 public class CustomSubscribeFilter : SubscribeFilter
 {
+    /// <summary>
+    /// 订阅方法执行前
+    /// </summary>
     public override void OnSubscribeExecuting(ExecutingContext context)
     {
-        // 订阅方法执行前
     }
 
+    /// <summary>
+    /// 订阅方法执行后
+    /// </summary>
     public override void OnSubscribeExecuted(ExecutedContext context)
     {
-        // 订阅方法执行后
     }
 
+    /// <summary>
+    /// 订阅方法执行异常
+    /// </summary>
     public override void OnSubscribeException(ExceptionContext context)
     {
-        // 订阅方法执行异常
+        //忽略异常
+        context.ExceptionHandled = true;
     }
 }
--- src/Berry.Spider.EventBus.RabbitMq/SpiderEventBusRabbitMqModule.cs
+++ src/Berry.Spider.EventBus.RabbitMq/SpiderEventBusRabbitMqModule.cs
@@ -19,6 +19,14 @@ public class SpiderEventBusRabbitMqModule : AbpModule
         {
             opt.UseEntityFramework<SpiderDbContext>();
             opt.UseDashboard();
+            //消费者线程并行处理消息的线程数
+            opt.ConsumerThreadCount = 5;
+            //如果设置为 true，则每个消费者组都会根据 ConsumerThreadCount 设置的值创建单独的线程进行处理。
+            opt.UseDispatchingPerGroup = true;
+            //失败消息的过期时间（秒）
+            opt.FailedMessageExpiredAfter = 30 * 24 * 3600;
+            //成功消息的过期时间（秒）
+            opt.SucceedMessageExpiredAfter = 7 * 24 * 3600;
 
             opt.UseRabbitMQ(o =>
             {
