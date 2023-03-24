21725d6dec6dc4ff271991c6e751c76215d93ce0
大師兄丶 <zhaoyi@yzw.cn>
2023-03-24 13:05:33 +0800

    feat: 移除无效代码

--- src/Berry.Spider.Tools.ServDetector/SpiderServDetectorModule.cs
+++ src/Berry.Spider.Tools.ServDetector/SpiderServDetectorModule.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Volo.Abp;
+﻿using Volo.Abp;
 using Volo.Abp.Autofac;
 using Volo.Abp.BackgroundWorkers;
 using Volo.Abp.BackgroundWorkers.Quartz;
@@ -18,12 +14,6 @@ public class SpiderServDetectorModule : AbpModule
 {
     public override async Task OnApplicationInitializationAsync(ApplicationInitializationContext context)
     {
-        var logger = context.ServiceProvider.GetRequiredService<ILogger<SpiderServDetectorModule>>();
-        var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
-
-        var hostEnvironment = context.ServiceProvider.GetRequiredService<IHostEnvironment>();
-        logger.LogInformation($"EnvironmentName => {hostEnvironment.EnvironmentName}");
-
         //注册服务
         await context.AddBackgroundWorkerAsync<ServLifetimeCheckerWorker>();
     }
