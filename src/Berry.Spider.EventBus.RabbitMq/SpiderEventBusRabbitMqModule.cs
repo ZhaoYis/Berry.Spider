0fe58e91cae8db6e5218aabc4e8a22d387659248
大師兄丶 <zhaoyi_dsx@163.com>
2023-10-29 20:55:58 +0800

    feat: 重构配置代码

--- src/Berry.Spider.EventBus.RabbitMq/SpiderEventBusRabbitMqModule.cs
+++ src/Berry.Spider.EventBus.RabbitMq/SpiderEventBusRabbitMqModule.cs
@@ -1,4 +1,5 @@
 using Berry.Spider.Contracts;
+using DotNetCore.CAP;
 using DotNetCore.CAP.Internal;
 using DotNetCore.CAP.Messages;
 using Microsoft.Extensions.Configuration;
@@ -16,83 +17,138 @@ public class SpiderEventBusRabbitMqModule : AbpModule
 
         context.Services.AddCap(opt =>
         {
-            MongoDBOptions? mongoDbOptions = configuration.GetSection(nameof(MongoDBOptions)).Get<MongoDBOptions>();
-            if (mongoDbOptions is not null)
-            {
-                opt.UseMongoDB(o =>
-                {
-                    o.DatabaseConnection = mongoDbOptions.ConnectionString;
-                    o.DatabaseName = "cap";
-                    o.ReceivedCollection = "cap.received";
-                    o.PublishedCollection = "cap.published";
-                    //o.LockCollection = "cap.lock";
-                });
-
-                //opt.UseStorageLock = true;
-            }
-
+            //配置数据存储方式
+            this.ConfigureDatabase(opt, configuration);
+            //注册节点到Consul
+            this.ConfigureConsul(opt, configuration);
+            //配置消费者
+            this.ConfigureConsumer(opt, configuration);
+            //配置RabbitMQ
+            this.ConfigureRabbitMq(opt, configuration);
             //使用管理面板
             opt.UseDashboard();
-
-            //注册节点到Consul
-            ConsulOptions? consulOptions = configuration.GetSection(nameof(ConsulOptions)).Get<ConsulOptions>();
-            if (consulOptions is { IsEnabled: true })
-            {
-                string nodeId = Guid.NewGuid().ToString("N");
-                string nodeName = "Consumer_" + nodeId;
-                opt.UseConsulDiscovery(d =>
-                {
-                    d.DiscoveryServerHostName = consulOptions.DiscoveryServerHostName;
-                    d.DiscoveryServerPort = consulOptions.DiscoveryServerPort;
-                    d.NodeId = nodeId;
-                    d.NodeName = nodeName;
-                    d.CustomTags = new[] { "Berry_Spider_Consumer" };
-                });
-            }
-
-            ConsumerOptions? consumerOptions = configuration.GetSection(nameof(ConsumerOptions)).Get<ConsumerOptions>();
-            if (consumerOptions is not null)
-            {
-                //消费者线程并行处理消息的线程数
-                opt.ConsumerThreadCount = consumerOptions.ConsumerThreadCount;
-                //如果设置为 true，则每个消费者组都会根据 ConsumerThreadCount 设置的值创建单独的线程进行处理。
-                // opt.UseDispatchingPerGroup = true;
-                opt.EnableConsumerPrefetch = consumerOptions.EnableConsumerPrefetch;
-            }
-
-            //失败消息的过期时间（秒）
-            opt.FailedMessageExpiredAfter = 30 * 24 * 3600;
-            //成功消息的过期时间（秒）
-            opt.SucceedMessageExpiredAfter = 7 * 24 * 3600;
-
-            EventBusRabbitMqOptions? rabbitMqOptions =
-                configuration.GetSection("RabbitMQ:Connections:Default").Get<EventBusRabbitMqOptions>();
-            if (rabbitMqOptions is not null)
-            {
-                opt.UseRabbitMQ(o =>
-                {
-                    o.HostName = rabbitMqOptions.HostName;
-                    o.UserName = rabbitMqOptions.UserName;
-                    o.Password = rabbitMqOptions.Password;
-                    o.Port = Convert.ToInt32(rabbitMqOptions.Port);
-                    o.VirtualHost = rabbitMqOptions.VirtualHost;
-
-                    o.CustomHeadersBuilder = (eventArgs, provider) =>
-                    {
-                        ISnowflakeId snowflakeId = provider.GetRequiredService<ISnowflakeId>();
-                        return new List<KeyValuePair<string, string>>
-                        {
-                            new(Headers.MessageId, snowflakeId.NextId().ToString()),
-                            new(Headers.MessageName, eventArgs.RoutingKey)
-                        };
-                    };
-
-                    o.PublishConfirms = rabbitMqOptions.PublishConfirms;
-                });
-            }
         }).AddSubscribeFilter<CustomSubscribeFilter>();
 
         //注册RabbitMq消息发布器
         context.Services.AddTransient<IEventBusPublisher, RabbitMqEventBusPublisher>();
+    }
+
+    /// <summary>
+    /// 配置RabbitMQ
+    /// </summary>
+    private void ConfigureRabbitMq(CapOptions opt, IConfiguration configuration)
+    {
+        EventBusRabbitMqOptions? rabbitMqOptions =
+            configuration.GetSection("RabbitMQ:Connections:Default").Get<EventBusRabbitMqOptions>();
+        if (rabbitMqOptions is not null)
+        {
+            opt.UseRabbitMQ(o =>
+            {
+                o.HostName = rabbitMqOptions.HostName;
+                o.UserName = rabbitMqOptions.UserName;
+                o.Password = rabbitMqOptions.Password;
+                o.Port = Convert.ToInt32(rabbitMqOptions.Port);
+                o.VirtualHost = rabbitMqOptions.VirtualHost;
+
+                o.CustomHeadersBuilder = (eventArgs, provider) =>
+                {
+                    ISnowflakeId snowflakeId = provider.GetRequiredService<ISnowflakeId>();
+                    return new List<KeyValuePair<string, string>>
+                    {
+                        new(Headers.MessageId, snowflakeId.NextId().ToString()),
+                        new(Headers.MessageName, eventArgs.RoutingKey)
+                    };
+                };
+
+                o.PublishConfirms = rabbitMqOptions.PublishConfirms;
+            });
+        }
+    }
+
+    /// <summary>
+    /// 配置消费者
+    /// </summary>
+    private void ConfigureConsumer(CapOptions opt, IConfiguration configuration)
+    {
+        ConsumerOptions? consumerOptions = configuration.GetSection(nameof(ConsumerOptions)).Get<ConsumerOptions>();
+        if (consumerOptions is not null)
+        {
+            //消费者线程并行处理消息的线程数
+            opt.ConsumerThreadCount = consumerOptions.ConsumerThreadCount;
+            //如果设置为 true，则每个消费者组都会根据 ConsumerThreadCount 设置的值创建单独的线程进行处理。
+            // opt.UseDispatchingPerGroup = true;
+            opt.EnableConsumerPrefetch = consumerOptions.EnableConsumerPrefetch;
+        }
+
+        //失败消息的过期时间（秒）
+        opt.FailedMessageExpiredAfter = 30 * 24 * 3600;
+        //成功消息的过期时间（秒）
+        opt.SucceedMessageExpiredAfter = 7 * 24 * 3600;
+    }
+
+    /// <summary>
+    /// 注册节点到Consul
+    /// </summary>
+    private void ConfigureConsul(CapOptions opt, IConfiguration configuration)
+    {
+        ConsulOptions? consulOptions = configuration.GetSection(nameof(ConsulOptions)).Get<ConsulOptions>();
+        if (consulOptions is {IsEnabled: true})
+        {
+            string nodeId = Guid.NewGuid().ToString("N");
+            string nodeName = "Consumer_" + nodeId;
+            opt.UseConsulDiscovery(d =>
+            {
+                d.DiscoveryServerHostName = consulOptions.DiscoveryServerHostName;
+                d.DiscoveryServerPort = consulOptions.DiscoveryServerPort;
+                d.NodeId = nodeId;
+                d.NodeName = nodeName;
+                d.CustomTags = new[] {"Berry_Spider_Consumer"};
+            });
+        }
+    }
+
+    /// <summary>
+    /// 配置数据存储方式
+    /// </summary>
+    private void ConfigureDatabase(CapOptions opt, IConfiguration configuration)
+    {
+        if (!this.UseMongoDb(opt, configuration))
+        {
+            this.UsePostgreSQL(opt, configuration);
+        }
+    }
+
+    /// <summary>
+    /// 使用MongoDB作为底板
+    /// </summary>
+    private bool UseMongoDb(CapOptions opt, IConfiguration configuration)
+    {
+        MongoDBOptions? mongoDbOptions = configuration.GetSection(nameof(MongoDBOptions)).Get<MongoDBOptions>();
+        if (mongoDbOptions is not null and {IsEnabled: true})
+        {
+            opt.UseMongoDB(o =>
+            {
+                o.DatabaseConnection = mongoDbOptions.ConnectionString;
+                o.DatabaseName = "cap";
+                o.ReceivedCollection = "cap.received";
+                o.PublishedCollection = "cap.published";
+                //o.LockCollection = "cap.lock";
+            });
+
+            //opt.UseStorageLock = true;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// 使用PostgreSQL作为底板
+    /// </summary>
+    private bool UsePostgreSQL(CapOptions opt, IConfiguration configuration)
+    {
+        //TODO
+        return false;
     }
 }
