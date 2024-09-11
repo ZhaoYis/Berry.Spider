212ce639419394e076d90e8ed5f34ee7fa99d7fd
大師兄丶 <zhaoyi_dsx@163.com>
2024-09-11 22:02:08 +0800

    feat: 绘制主页面

--- desktop/Berry.Spider.ToolkitStore/ViewModels/MainWindowViewModel.cs
+++ desktop/Berry.Spider.ToolkitStore/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,74 @@
-﻿namespace Berry.Spider.ToolkitStore.ViewModels;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Berry.Spider.ToolkitStore.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    /// <summary>
+    /// 启动程序路径
+    /// </summary>
+    [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(AppRunCommand))]
+    private string? _appStrapPath;
+
+    /// <summary>
+    /// 程序是否运行中
+    /// </summary>
+    [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(AppRunCommand))]
+    private bool _isRunning = false;
+
+    /// <summary>
+    /// 选择启动程序
+    /// </summary>
+    [RelayCommand]
+    private async Task AppSelectorAsync()
+    {
+        var desktop = Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+        var storage = desktop?.MainWindow?.StorageProvider;
+        if (storage is not null)
+        {
+            var files = await storage.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                AllowMultiple = false,
+                FileTypeFilter = new[] { FilePickerFileTypes.TextPlain }
+            });
+
+            if (files is { Count: > 0 })
+            {
+                this.AppStrapPath = files[0].Path.ToString();
+            }
+        }
+
+        await Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 启动
+    /// </summary>
+    [RelayCommand(CanExecute = nameof(AppRunCanExecute))]
+    private async Task AppRunAsync()
+    {
+        this.IsRunning = true;
+        await Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 取消
+    /// </summary>
+    [RelayCommand]
+    private async Task AppCancleAsync()
+    {
+        this.IsRunning = false;
+        await Task.CompletedTask;
+    }
+
+    private bool AppRunCanExecute()
+    {
+        return this.IsRunning == false && this.AppStrapPath is not null;
+    }
 }
