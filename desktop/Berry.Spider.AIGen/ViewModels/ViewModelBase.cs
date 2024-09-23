a0546bfc385a5b660025fcc80d56ba6767d72fac
大師兄丶 <zhaoyi_dsx@163.com>
2024-09-23 20:17:08 +0800

    feat: 初始化AIGen项目

---
+++ desktop/Berry.Spider.AIGen/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Berry.Spider.AIGen.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ desktop/Berry.Spider.AIGen/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Berry.Spider.AIGen.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
