81c278332230370369fd62da395bc8a8ebc882a1
大師兄丶 <zhaoyi@yzw.cn>
2024-09-24 10:08:21 +0800

    feat：优化配置读取逻辑

--- desktop/Berry.Spider.AIGen/SpiderAIGenModule.cs
+++ desktop/Berry.Spider.AIGen/SpiderAIGenModule.cs
@@ -3,10 +3,12 @@ using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using AgileConfig.Client;
 using Berry.Spider.AIGen.Views;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
+using Volo.Abp.Threading;
 
 namespace Berry.Spider.AIGen;
 
@@ -16,35 +18,37 @@ public class SpiderAIGenModule : AbpModule
     [Experimental("SKEXP0010")]
     public override void PreConfigureServices(ServiceConfigurationContext context)
     {
-        var appId = "berry_spider_consumers";
-        var secret = "1q2w3E*.com";
-        var nodes = "http://124.223.62.114:15000";
-        ConfigClient client = new ConfigClient(appId, secret, nodes, "PROD")
+        var configuration = context.Services.GetConfiguration();
+        ConfigClientOptions? configClientOptions = configuration.GetSection(nameof(ConfigClientOptions)).Get<ConfigClientOptions>();
+        if (configClientOptions is not null)
         {
-            Name = "Berry.Spider.AIGen",
-            Tag = ""
-        };
-        client.ConnectAsync().GetAwaiter().GetResult();
+            ConfigClient client = new ConfigClient(configClientOptions);
+            bool isConnect = AsyncHelper.RunSync(async () => await client.ConnectAsync());
+            if (isConnect)
+            {
+                //注入SK核心Kernel服务
+                string modelId = client.Get("OllamaQwenOptions:ModelId");
+                string serviceAddr = client.Get("OllamaOptions:ServiceAddr");
+                context.Services.AddTransient(serviceProvider =>
+                {
+                    var handler = new OpenAIHttpClientHandler(serviceAddr);
+                    var kernel = Kernel.CreateBuilder()
+                        .AddOpenAIChatCompletion(modelId: modelId,
+                            endpoint: new Uri(serviceAddr),
+                            apiKey: null,
+                            httpClient: new HttpClient(handler))
+                        .Build();
+                    return kernel;
+                });
 
-        //注入SK核心Kernel服务
-        string modelId = client.Get("OllamaQwenOptions:ModelId");
-        string serviceAddr = client.Get("OllamaOptions:ServiceAddr");
-        context.Services.AddTransient(serviceProvider =>
-        {
-            var handler = new OpenAIHttpClientHandler(serviceAddr);
-            var kernel = Kernel.CreateBuilder()
-                .AddOpenAIChatCompletion(modelId: modelId,
-                    endpoint: new Uri(serviceAddr),
-                    apiKey: null, httpClient: new HttpClient(handler))
-                .Build();
-            return kernel;
-        });
-
-        context.Services.AddSingleton(client);
+                context.Services.AddSingleton(client);
+            }
+        }
     }
 
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
+        var configuration = context.Services.GetConfiguration();
         context.Services.AddSingleton<MainWindow>();
     }
 }
