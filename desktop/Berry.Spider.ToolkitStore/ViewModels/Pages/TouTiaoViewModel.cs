00e8cd698d4936ca5371536c79fe046f00d93318
大師兄丶 <zhaoyi_dsx@163.com>
2025-01-20 21:01:01 +0800

    feat：优化逻辑

--- desktop/Berry.Spider.ToolkitStore/ViewModels/Pages/TouTiaoViewModel.cs
+++ desktop/Berry.Spider.ToolkitStore/ViewModels/Pages/TouTiaoViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Platform.Storage;
 using Berry.Spider.Core;
@@ -14,6 +15,7 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OpenQA.Selenium;
+using Volo.Abp;
 using Volo.Abp.DependencyInjection;
 
 namespace Berry.Spider.ToolkitStore.ViewModels.Pages;
@@ -31,12 +33,17 @@ public partial class TouTiaoViewModel : ViewModelBase, ITransientDependency
     [NotifyCanExecuteChangedFor(nameof(ExecCommand))] [ObservableProperty]
     private string _currentFilePath;
 
+    /// <summary>
+    /// 异步任务取消令牌
+    /// </summary>
+    private readonly CancellationTokenSource cancellationTokenSource = new();
+
     private IWebElementLoadProvider WebElementLoadProvider { get; }
     private IResolveJumpUrlProvider ResolveJumpUrlProvider { get; }
     private ILogger<TouTiaoViewModel> Logger { get; }
     private IMediator Mediator { get; }
     private string HomePage => "https://so.toutiao.com/search?keyword={0}&pd=question&dvpf=pc";
-    
+
     public TouTiaoViewModel(IServiceProvider serviceProvider,
                             IWebElementLoadProvider webProvider,
                             ILogger<TouTiaoViewModel> logger,
@@ -82,73 +89,64 @@ public partial class TouTiaoViewModel : ViewModelBase, ITransientDependency
     {
         if (!File.Exists(this.CurrentFilePath)) return;
 
-        var lines = await File.ReadAllLinesAsync(this.CurrentFilePath);
-        if (lines is { Length: > 0 })
+        string saveFileName = $"{DateTime.Now:yyyyMMddHHmmssfff}.txt";
+        string saveFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, saveFileName);
+
+        IAsyncEnumerable<string> lines = File.ReadLinesAsync(this.CurrentFilePath);
+        await Parallel.ForEachAsync(lines, new ParallelOptions
         {
-            string saveFileName = $"{DateTime.Now:yyyyMMddHHmmssfff}.txt";
-            string saveFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, saveFileName);
+            MaxDegreeOfParallelism = AppGlobalConstants.ParallelSafeDegreeOfParallelism,
+            CancellationToken = cancellationTokenSource.Token
+        }, async (_keyword, _cancellationToken) =>
+        {
+            if (string.IsNullOrEmpty(_keyword)) return;
+            this.SetExecLog($"开始抓取关键字：{_keyword}");
 
-            await Parallel.ForEachAsync(lines, new ParallelOptions
+            string targetUrl = string.Format(this.HomePage, _keyword);
+            await this.WebElementLoadProvider.InvokeAsync(targetUrl, _keyword, drv => drv.FindElement(By.CssSelector(".s-result-list")), async (root, state) =>
             {
-                MaxDegreeOfParallelism = AppGlobalConstants.ParallelSafeDegreeOfParallelism
-            }, async (keyword, token) =>
-            {
-                if (string.IsNullOrEmpty(keyword)) return;
-                this.SetExecLog($"开始抓取关键字：{keyword}");
+                if (root == null) return;
+                this.SetExecLog($"抓取到页面数据，开始执行数据解析操作...");
 
-                string targetUrl = string.Format(this.HomePage, keyword);
-                await this.WebElementLoadProvider.InvokeAsync(targetUrl, keyword, drv => drv.FindElement(By.CssSelector(".s-result-list")), async (root, _keyword) =>
+                var resultContent = root.TryFindElements(By.CssSelector(".result-content"));
+                if (resultContent is null or { Count: 0 }) return;
+
+                var resultBag = new ConcurrentBag<ChildPageDataItem>();
+                await Parallel.ForEachAsync(resultContent, new ParallelOptions
                 {
-                    if (root == null) return;
-                    this.SetExecLog($"抓取到页面数据，开始执行数据解析操作...");
-
-                    var resultContent = root.TryFindElements(By.CssSelector(".result-content"));
-                    if (resultContent is null or { Count: 0 }) return;
-
-                    var resultBag = new ConcurrentBag<ChildPageDataItem>();
-                    await Parallel.ForEachAsync(resultContent, new ParallelOptions
+                    MaxDegreeOfParallelism = AppGlobalConstants.ParallelMaxDegreeOfParallelism,
+                    CancellationToken = _cancellationToken
+                }, async (element, _) =>
+                {
+                    var a = element.TryFindElement(By.TagName("a"));
+                    if (a != null)
                     {
-                        MaxDegreeOfParallelism = AppGlobalConstants.ParallelMaxDegreeOfParallelism
-                    }, async (element, _) =>
-                    {
-                        var a = element.TryFindElement(By.TagName("a"));
-                        if (a != null)
+                        string text = a.Text.Trim();
+                        string href = a.GetDomAttribute("href");
+
+                        if (!string.IsNullOrEmpty(text) && !string.IsNullOrEmpty(href))
                         {
-                            string text = a.Text.Trim();
-                            string href = a.GetDomAttribute("href");
                             string realHref = await this.ResolveJumpUrlProvider.ResolveAsync(href);
-
-                            if (!string.IsNullOrEmpty(text) && !string.IsNullOrEmpty(realHref))
+                            resultBag.Add(new ChildPageDataItem
                             {
-                                resultBag.Add(new ChildPageDataItem
-                                {
-                                    Title = text,
-                                    Href = realHref
-                                });
-                            }
-                        }
-                    });
-
-                    if (resultBag is { Count: > 0 })
-                    {
-                        try
-                        {
-                            List<string> titles = resultBag.Select(x => x.Title).ToList();
-                            //直接发送本地消息
-                            await this.Mediator.Send(new ChildPageTitleRequest(saveFilePath, titles), token);
-                            this.SetExecLog($"关键字：{keyword}，保存采集到的标题：{resultBag.Count}条{Environment.NewLine}");
-                        }
-                        catch (Exception e)
-                        {
-                            this.Logger.LogException(e);
-                            this.SetExecLog($"[发生异常]{e.ToString()}");
+                                Title = text,
+                                Href = realHref
+                            });
                         }
                     }
                 });
 
-                await Task.Delay(1000, token);
+                if (resultBag is { Count: > 0 })
+                {
+                    List<string> titles = resultBag.Select(x => x.Title).ToList();
+                    //直接发送本地消息
+                    await this.Mediator.Send(new ChildPageTitleRequest(saveFilePath, titles), _cancellationToken);
+                    this.SetExecLog($"关键字：{state}，保存采集到的标题：{resultBag.Count}条{Environment.NewLine}");
+                }
             });
-        }
+
+            await Task.Delay(RandomHelper.GetRandom(500, 1000), _cancellationToken);
+        });
     }
 
     private bool CanExecute()
