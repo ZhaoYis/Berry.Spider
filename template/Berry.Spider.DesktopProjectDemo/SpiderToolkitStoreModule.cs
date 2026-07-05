af94e66f860a8868d14b5d2798322948d80333a0
大師兄丶 <zhaoyi_dsx@163.com>
2026-07-05 12:02:51 +0800

    feat：调整模版项目

--- template/Berry.Spider.DesktopProjectDemo/SpiderToolkitStoreModule.cs
+++ template/Berry.Spider.DesktopProjectDemo/SpiderToolkitStoreModule.cs
@@ -1,10 +1,7 @@
-using AgileConfig.Client;
 using Berry.Spider.ToolkitStore.Views;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
-using Volo.Abp.Threading;
 
 namespace Berry.Spider.ToolkitStore;
 
@@ -14,16 +11,6 @@ public class SpiderToolkitStoreModule : AbpModule
     public override void PreConfigureServices(ServiceConfigurationContext context)
     {
         var configuration = context.Services.GetConfiguration();
-        ConfigClientOptions? configClientOptions = configuration.GetSection(nameof(ConfigClientOptions)).Get<ConfigClientOptions>();
-        if (configClientOptions is not null)
-        {
-            ConfigClient client = new ConfigClient(configClientOptions);
-            bool isConnect = AsyncHelper.RunSync(async () => await client.ConnectAsync());
-            if (isConnect)
-            {
-                context.Services.AddSingleton<ConfigClient>(client);
-            }
-        }
     }
 
     public override void ConfigureServices(ServiceConfigurationContext context)
